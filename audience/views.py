d83971124c59f3e5a27a5256e87e5884fbf6ee97
TAGCH <pisit.au@ku.th>
2023-11-10 18:02:04 +0700

    fixed

--- audience/views.py
+++ audience/views.py
@@ -5,12 +5,26 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 def homepage(request):
     data = {
-        "Arsenal": {
-            "rank": 1
-        },
-        "Aston Villa": {
-            "rank": 2
-        }
+        "1": "Arsenal",
+        "2": "Aston Villa",
+        "3": "Bournemouth",
+        "4": "Brentford",
+        "5": "Brighton & Hove Albion",
+        "6": "Burnley",
+        "7": "Chelsea",
+        "8": "Crystal Palace",
+        "9": "Everton",
+        "10": "Fulham",
+        "11": "Liverpool",
+        "12": "Luton Town",
+        "13": "Manchester City",
+        "14": "Manchester United",
+        "15": "Newcastle United",
+        "16": "Nottingham Forest",
+        "17": "Sheffield United",
+        "18": "Tottenham Hotspur",
+        "19": "West Ham United",
+        "20": "Wolverhampton Wanderers"
     }
     context = {"page": "homepage", "detail": "show all team", "data": data}
     return render(request, "audience/homepage.html", context)
@@ -21,8 +35,8 @@ def team_detail(request, team_id):
         "team_name": "Arsenal",
         "rank": 1,
         "win": 10,
-        "Draw": 10,
-        "Player": [
+        "draw": 10,
+        "player": [
             {
                 "player_code": "A",
                 "number": 10,
